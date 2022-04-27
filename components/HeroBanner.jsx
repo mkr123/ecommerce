3115e6031ecbc8b7896a18bcbde07168196413fb
mkr123 <mankairui@yahoo.com>
2022-04-26 20:31:21 -0700

    hook up with sanity

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from 'next/link'
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+  <div className = 'hero-banner-container'>
+    <div>
+      <p className="beats-solo">{heroBanner.smallText}</p>
+      <h3>{heroBanner.midText}</h3>
+      <img
+      src={heroBanner.image}
+      alt="headphones"
+      className = "hero-banner-image"
+      />
+      <div>
+        <Link href="/product/ID" >
+          <button type = "button">{heroBanner.buttonText}</button>
+        </Link>
+        <div className="desc">
+            <h5>Descrisption</h5>
+            <p>{heroBanner.desc}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+};
+
+export default HeroBanner;
